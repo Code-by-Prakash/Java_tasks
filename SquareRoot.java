68e34db5c306509c3abd8c63f62c1f3e9be52cbf
PRAKASH P <107047453+Code-by-Prakash@users.noreply.github.com>
2024-03-15 14:20:55 +0530

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -5,8 +5,8 @@ public class SquareRoot
     public static void main(String[] args)
     {
       Scanner s= new Scanner(System.in);
-      int num=s.nextInt();
-      Math m = new Math();
-      m.sqrt(num);
+      System.out.println("Enter a number:");
+      float num=s.nextInt();
+     System.out.println(Math.sqrt(num));
     }
   }
