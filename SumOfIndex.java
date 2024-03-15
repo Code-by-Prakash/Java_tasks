1b4f728ecab082bc93f32d37030e83339df6e818
PRAKASH P <107047453+Code-by-Prakash@users.noreply.github.com>
2024-03-15 14:23:46 +0530

    Update SumOfIndex.java

--- SumOfIndex.java
+++ SumOfIndex.java
@@ -1,13 +1,11 @@
-public class SumOfIndex
-{
-  public static void main(String[] args)
-  {
-    String s= "Prakash";
-    int sum=0;
-    for(int i=0;i<s.length();i++)
-      {
-        sum+=i;
-        
-  }
-    System.out.println(sum)
-      }
+public class SumOfIndex {
+	public static void main(String[] args) {
+		String s = "Prakash";
+		int sum = 0;
+		for (int i = 0; i < s.length(); i++) {
+			sum += i;
+
+		}
+		System.out.println(sum);
+	}
+}
