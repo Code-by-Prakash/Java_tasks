b0723d5d9b237427f5246919044fc42c79c722d8
PRAKASH P <107047453+Code-by-Prakash@users.noreply.github.com>
2024-03-15 14:04:12 +0530

    Create SquareRoot.java

---
+++ SquareRoot.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class SquareRoot
+  {
+    public static void main(String[] args)
+    {
+      Scanner s= new Scanner(System.in);
+      int num=s.nextInt();
+      Math m = new Math();
+      m.sqrt(num);
+    }
+  }
