38bd000cdc88b24a2c35de33634f06a1e57981ae
PRAKASH P <107047453+Code-by-Prakash@users.noreply.github.com>
2024-03-15 14:00:45 +0530

    Create ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,11 @@
+public class ReverseArray
+  {
+    public static void main(String[] args)
+    {
+      int a[]=[1,4,2,4,2];
+      for(int i=a.length-1;i>0;i--)
+        {
+          System.out.println("Reversed elements of an array:"+a[i]);
+        }
+    }
+  }
