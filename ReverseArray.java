c70ab02ace426e7f2ff3fa195ea926d5d75b2da3
PRAKASH P <107047453+Code-by-Prakash@users.noreply.github.com>
2024-03-15 14:15:39 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,11 +1,12 @@
 public class ReverseArray
+{
+  public static void main(String[] args)
   {
-    public static void main(String[] args)
-    {
-      int a[]=[1,4,2,4,2];
-      for(int i=a.length-1;i>0;i--)
-        {
-          System.out.println("Reversed elements of an array:"+a[i]);
-        }
-    }
+    int a[]= {1,4,2,4,2};
+    System.out.println("Reversed elements of an array:");
+    for(int i=a.length-1;i>=0;i--)
+      {
+        System.out.print(a[i]);
+      }
   }
+}
